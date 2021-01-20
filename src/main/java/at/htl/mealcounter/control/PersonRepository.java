5b39bbd1ccd7226ff094984b621b14a6ef84b285
sandy <sandytang2406@gmail.com>
2021-01-20 21:57:05 +0100

    PersonRepository findById and save method add

--- src/main/java/at/htl/mealcounter/control/PersonRepository.java
+++ src/main/java/at/htl/mealcounter/control/PersonRepository.java
@@ -2,24 +2,33 @@ package at.htl.mealcounter.control;
 
 import at.htl.mealcounter.entity.Person;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.util.List;
 
 
+@ApplicationScoped
 public class PersonRepository {
 
+    @Inject
+    EntityManager em;
+
     public Person findById(String id) {
-        return null;
+       return em.find(Person.class,id);
     }
 
     public List<Person> findAll() {
         return null;
     }
 
+    @Transactional
     public Person save(Person person) {
-        return null;
+        return em.merge(person);
     }
 
+    @Transactional
     public void delete(long id) {
-
     }
 }
