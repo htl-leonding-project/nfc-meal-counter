20cd62feb6bcbf4b4f097b6a545c1881186790d2
kellytran03 <43615496+kellytran03@users.noreply.github.com>
2021-01-21 20:07:31 +0100

    ConsumationRepositoryTest save() Method implemented #8

--- src/test/java/at/htl/mealcounter/control/ConsumationRepositoryTest.java
+++ src/test/java/at/htl/mealcounter/control/ConsumationRepositoryTest.java
@@ -2,38 +2,55 @@ package at.htl.mealcounter.control;
 
 import at.htl.mealcounter.entity.Consumation;
 import at.htl.mealcounter.entity.Person;
+import io.quarkus.test.junit.QuarkusTest;
+import org.assertj.db.api.Assertions;
+import org.assertj.db.output.impl.Output;
+import org.assertj.db.type.Table;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.assertj.db.type.Table;
+import static org.assertj.core.api.Assertions.*;
+
+import static org.assertj.db.api.Assertions.*;
 
 
+import static org.assertj.db.output.Outputs.output;
 import static org.junit.jupiter.api.Assertions.*;
 import javax.inject.Inject;
+import javax.sql.DataSource;
+
 import java.time.LocalDate;
 
-
+@QuarkusTest
 class ConsumationRepositoryTest {
 
     @Inject
     ConsumationRepository consumationRepository;
 
+    private static DataSource  dataSource;
+
+    @BeforeAll
+    static void Init() {
+        dataSource = DatabaseHelper.getDatasource();
+    }
+
     @Test
     @Order(1)
     void save() {
 
-        Person person = new Person("Brooke", "Bradford", 2020, "1a");
+        Person person = new Person("Michelle", "Obama", 2021, "4a");
 
         Consumation consumation = new Consumation(person, LocalDate.of(2021,1,8),true);
-        consumationRepository.save(consumation);
 
-        Table table = new Table(Database.getDataSource(), "answer_option");
+        consumation = consumationRepository.save(consumation);
 
-        Assertions.assertThat(table).row(0)
-                .value("ao_text").isEqualTo("völlig zu")
-                .value("ao_value").isEqualTo(4)
-                .value("ao_q_id").isEqualTo(1);
+        Table table = new Table(dataSource, DatabaseHelper.CONSUMATION_TABLE);
+        output(table).toConsole();
 
-
+        Assertions.assertThat(table).row(3)
+                .value()
+                .value("DATE").isEqualTo(consumation.getDate())
+                .value("HASCONSUMED").isEqualTo(consumation.isHasConsumed());
     }
 
 }
