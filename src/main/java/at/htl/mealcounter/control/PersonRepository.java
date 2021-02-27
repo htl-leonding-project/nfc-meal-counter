42131c9da92fa6da4e2acee5a8be352917190f29
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2021-02-27 21:27:22 +0100

    add readFromCsv method #12

--- src/main/java/at/htl/mealcounter/control/PersonRepository.java
+++ src/main/java/at/htl/mealcounter/control/PersonRepository.java
@@ -6,7 +6,16 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.lang.System.out;
 
 
 @ApplicationScoped
@@ -31,5 +40,25 @@ public class PersonRepository {
     @Transactional
     public void delete(long id) {
         em.remove(findById(id));
+    }
+
+
+    @Transactional
+    public void readFromCsv() {
+
+        URL url = Thread.currentThread().getContextClassLoader().getResource("names.csv");
+        try (Stream<String> stream = Files.lines(Paths.get(url.getPath()), StandardCharsets.UTF_8)) {
+            stream.skip(1)
+                    .map(s -> s.split(";"))
+                    .map(a -> new Person(
+                            a[1],
+                            a[2],
+                            Integer.parseInt(String.valueOf(LocalDate.now().getYear())),
+                            a[0]))
+                    .peek(out::println)
+                    .forEach(em::merge);// was heißt das
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
