c526ba94dd69616c0ac0b5adc09bfb03d36234b3
sandy <sandytang2406@gmail.com>
2021-06-18 09:53:53 +0200

    add NfcCard #3

---
+++ src/main/java/at/htl/mealcounter/entity/NfcCard.java
@@ -0,0 +1,11 @@
+package at.htl.mealcounter.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class NfcCard {
+
+    public String nfcId;
+    public LocalDateTime registerDateTime;
+
+}
