179917989d6a47f6d2591c1a9c7a214ab9aeef68
sandy <sandytang2406@gmail.com>
2021-06-18 10:43:12 +0200

    add constructor,getter,setter #3

--- src/main/java/at/htl/mealcounter/entity/NfcCard.java
+++ src/main/java/at/htl/mealcounter/entity/NfcCard.java
@@ -8,4 +8,23 @@ public class NfcCard {
     public String nfcId;
     public LocalDateTime registerDateTime;
 
+    public NfcCard(String nfcId) {
+        this.nfcId = nfcId;
+    }
+
+    public String getNfcId() {
+        return nfcId;
+    }
+
+    public void setNfcId(String nfcId) {
+        this.nfcId = nfcId;
+    }
+
+    public LocalDateTime getRegisterDateTime() {
+        return registerDateTime;
+    }
+
+    public void setRegisterDateTime(LocalDateTime registerDateTime) {
+        this.registerDateTime = registerDateTime;
+    }
 }
