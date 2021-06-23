1fd0849c117e31eebd196485fc5b0a9609afa596
sandy <sandytang2406@gmail.com>
2021-06-23 13:06:22 +0200

    adjust class NfcCard #3

--- src/main/java/at/htl/mealcounter/entity/NfcCard.java
+++ src/main/java/at/htl/mealcounter/entity/NfcCard.java
@@ -2,16 +2,22 @@ package at.htl.mealcounter.entity;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "M_NFC_CARD")
 public class NfcCard extends PanacheEntity {
 
     @Id
+    @Column(name = "NFC_ID")
     public String nfcId;
+
+    @Column(name = "REGISTER_DATE_TIME")
     public LocalDateTime registerDateTime;
 
     public NfcCard(String nfcId) {
